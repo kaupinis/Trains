56cd6f9ac5224b5558eefe9c64a406edc3bfda55
kaupinis <wkaupinis@gmail.com>
2026-07-30 14:04:37 -0400

    initial eo_i18n_support.js

---
+++ webapp/js/eo_i18n_support.js
@@ -0,0 +1,27 @@
+// eo_i18n_support.js
+
+/** Creates a JSON object for a messages_lang.json file
+ * mode == 0 -> create complete data
+ * mode == 1 -> create only items not defined in current messages data.
+ */
+function makeMessagesTemplate(mode)
+{
+  let o = {};
+  document.querySelectorAll('[data-dxt]').forEach((el) => { // dit == data-i18n
+      const key = el.getAttribute('data-dxt');
+      if(mode == 0)
+      {
+        o[key] = el.textContent;
+      }
+      else if(mode == 1)
+      {
+        let r = eo_i18n.getMessage(key);
+        if((typeof r === 'undefined') || (r == null) || (r == ""))
+        {
+          o[key] = el.textContent;
+        }
+      }
+      });
+  clearReport();
+  report(JSON.stringify(o, null, 4));
+}
